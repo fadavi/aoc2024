a230b9fb4839b727d6665dc9b6e45fbcfc06e269
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2024-12-07 00:20:24 +0100

    feat(day02): solve day02 - part2!

--- src/day02.py
+++ src/day02.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from os import walk
+from collections.abc import Iterable
 from load_input import load_input
-from pprint import pprint
 
-Report = list[int]
+
 DELIM = " "
+MIN_SAFE_DIFF, MAX_SAFE_DIFF = 1, 3
 
+Level = int
+Report = list[Level]
 
 def main():
     input_str = load_input("day02.txt")
@@ -14,6 +16,9 @@ def main():
 
     safe_reports = count_safe_reports(reports)
     print("Part1 - # of Safe Reports:", safe_reports)
+
+    safe_reports_with_tolerance = count_safe_reports(reports, tolerance=1)
+    print("Part2 - # of Safe Reports with Tolerance:", safe_reports_with_tolerance)
 
 
 def parse_input(input_str: str):
@@ -27,22 +32,42 @@ def parse_input(input_str: str):
     return reports
 
 
-def is_safe(report):   
-    increasing = report[1] > report[0]
-
-    for cur, nxt in zip(report, report[1:]):
-        if increasing != (nxt > cur):
-            return False
-
-        delta = abs(cur - nxt)
-        if delta < 1 or delta > 3:
-            return False
-
-    return True
+def is_safe(report: Report, tolerance=0):
+    return (
+        is_increasing_safe(report, tolerance) or
+        is_increasing_safe(reversed(report), tolerance)
+    )
 
 
-def count_safe_reports(reports: list[Report]):
-    return sum(is_safe(report) for report in reports)
+def is_increasing_safe(report: Iterable[Level], tolerance=0):
+    levels = iter(report)
+    next_level = lambda: next(levels, None)
+
+    cur, nxt = next_level(), next_level()
+    head = True
+
+    while cur is not None and nxt is not None and tolerance >= 0:
+        if is_safe_diff(nxt - cur):
+            head = False
+            cur, nxt = nxt, next_level()
+            continue
+
+        tolerance -= 1
+
+        new_nxt = next_level()
+        if head and new_nxt is not None and not is_safe_diff(new_nxt - cur):
+            cur = nxt
+        nxt = new_nxt
+
+    return tolerance >= 0
+
+
+def is_safe_diff(diff: int):
+    return MIN_SAFE_DIFF <= diff <= MAX_SAFE_DIFF
+
+
+def count_safe_reports(reports: Iterable[Report], tolerance=0):
+    return sum(is_safe(report, tolerance) for report in reports)
 
 
 if __name__ == "__main__":
