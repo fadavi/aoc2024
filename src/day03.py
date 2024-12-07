8ac2eb3692bd948b23ca0b6715abfeb38516237a
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2024-12-07 01:28:35 +0100

    feat(day03): solve day03 - part2!

--- src/day03.py
+++ src/day03.py
@@ -1,33 +1,63 @@
 #!/usr/bin/env python
 
 from collections.abc import Iterable
+from functools import reduce
+from typing import Literal
+from itertools import tee
+from operator import mul
 import re
+
 from load_input import load_input
 
 
-MUL_PATTERN = r"mul\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)"
+MUL_PATTERN = r"mul\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)|(do\(\))|(don't\(\))"
+OPR1_GROUP, OPR2_GROUP, DO_GROUP, DONT_GROUP = 1, 2, 3, 4
+DO, DONT = "do()", "don't()"
+
 MulOperands = tuple[int, int]
+Instruction = MulOperands | Literal["do()", "don't()"]
+Instructions = Iterable[Instruction]
 
 
 def main():
-    input_str = load_input("day03.txt")
-    mul_operands = parse_mul_operands(input_str)
+    memory_dump = load_input("day03.txt")
+    insts1, insts2 = tee(parse_instructions(memory_dump))
 
-    mul_sum = calc_mul_sum(mul_operands)
-    print("Part1 - Sum of Mul operations:", mul_sum)
+    mul_sum_without_donts = sum_instructions_results(insts1)
+    print("Part1 - Sum of Mul operations:", mul_sum_without_donts)
 
-
-def parse_mul_operands(input_str: str) -> Iterable[MulOperands]:
-    for o1, o2 in re.findall(MUL_PATTERN, input_str):
-        yield int(o1), int(o2)
+    mul_sum_with_donts = sum_instructions_results(insts2, apply_donts=True)
+    print("Part2 - Sum of Mul operations with don'ts:", mul_sum_with_donts)
 
 
-def calc_mul(ops: MulOperands):
-    return ops[0] * ops[1]
+def parse_instructions(input_str: str) -> Instructions:
+    for m in re.finditer(MUL_PATTERN, input_str):
+        o1 = m.group(OPR1_GROUP)
+        o2 = m.group(OPR2_GROUP)
+
+        if o1 is not None and o2 is not None:
+            yield int(o1), int(o2)
+        elif m.group(DO_GROUP) == DO:
+            yield DO
+        elif m.group(DONT_GROUP) == DONT:
+            yield DONT
 
 
-def calc_mul_sum(ops: Iterable[MulOperands]):
-    return sum(calc_mul(ops) for ops in ops)
+def run_instructions(insts: Instructions, apply_donts=False):
+    enabled = True
+    for inst in insts:
+        if inst in (DO, DONT):
+            enabled = not apply_donts or inst == DO
+        elif enabled:
+            yield calc_mul(inst)
+
+
+def calc_mul(operands: MulOperands):
+    return reduce(mul, operands)
+
+
+def sum_instructions_results(insts: Instructions, apply_donts=False):
+    return sum(run_instructions(insts, apply_donts))
 
 
 if __name__ == "__main__":
