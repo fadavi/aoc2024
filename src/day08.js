5c4e82d947582b60cef95379d13b823de619565a
Mohamad Fadavi <fadavi.mohamad@gmail.com>
2024-12-08 08:47:44 +0100

    feat(day08): solve day08 part2 using js

--- src/day08.js
+++ src/day08.js
@@ -1,5 +1,7 @@
-const fs = require('fs')
+#!/usr/bin/env node
+
 const { join } = require('path')
+const fs = require('fs')
 
 const INPUT_PATH = join(__dirname, '..', 'inputs', 'day08.txt')
 const [X, Y] = [0, 1]
@@ -24,19 +26,6 @@ const appendAntenna = (antennas, freq, pos) => {
   } else {
     points.push(pos)
   }
-}
-
-/**
- * @param {Point} p1
- * @param {Point} p2
- */
-const getAntinodes = (p1, p2) => {
-  const dx = p2[X] - p1[X]
-  const dy = p2[Y] - p1[Y]
-  return [
-    [p1[X] - dx, p1[Y] - dy],
-    [p2[X] + dx, p2[Y] + dy],
-  ]
 }
 
 class AntennasMap {
@@ -82,7 +71,7 @@ class AntennasMap {
     return 0 <= x && x < this._width && 0 <= y && y < this._height
   }
 
-  countUniqueAntinodes() {
+  countUniqueAntinodes(repeat = false) {
     const antinodes = new Set()
 
     for (const freqPoints of this._antennas.values()) {
@@ -92,20 +81,51 @@ class AntennasMap {
         const pi = freqPoints[i]
         for (let j = i + 1; j < len; j++) {
           const pj = freqPoints[j]
-          const [an1, an2] = getAntinodes(pi, pj)
-
-          if (this.contains(an1)) {
-            antinodes.add(an1.join(','))
-          }
-
-          if (this.contains(an2)) {
-            antinodes.add(an2.join(','))
+          const ans = this._getAntinodes(pi, pj, repeat)
+          for (const an of ans) {
+            if (this.contains(an)) {
+              antinodes.add(an.join(','))
+            }
           }
         }
       }
     }
 
     return antinodes.size
+  }
+
+  /**
+   * @param {Point} p1
+   * @param {Point} p2
+   */
+  _getAntinodes(p1, p2, repeat = false) {
+    const dx = p2[X] - p1[X]
+    const dy = p2[Y] - p1[Y]
+
+    let a1x = p1[X] - dx
+    let a1y = p1[Y] - dy
+    let a2x = p2[X] + dx
+    let a2y = p2[Y] + dy
+
+    if (!repeat) {
+      return [[a1x, a1y], [a2x, a2y]]
+    }
+
+    const ans = [p1, p2]
+
+    while(this.contains([a1x, a1y])) {
+      ans.push([a1x, a1y])
+      a1x -= dx
+      a1y -= dy
+    }
+
+    while(this.contains([a2x, a2y])) {
+      ans.push([a2x, a2y])
+      a2x += dx
+      a2y += dy
+    }
+
+    return ans
   }
 }
 
@@ -115,6 +135,9 @@ const main = () => {
 
   const nAntinodes = map.countUniqueAntinodes()
   console.log('Part 1:', nAntinodes)
+
+  const nAntinodesRepeat = map.countUniqueAntinodes(true)
+  console.log('Part 2:', nAntinodesRepeat)
 }
 
 if (require.main === module) {
